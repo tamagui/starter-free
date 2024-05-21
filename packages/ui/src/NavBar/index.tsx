4ab93b3d5973978786090d12beb0ce851c934078
Daniagui12 <d.aguilera@uniandes.edu.co>
2024-05-20 21:04:46 -0500

    fukcing shit

--- packages/ui/src/NavBar/index.tsx
+++ packages/ui/src/NavBar/index.tsx
@@ -1,16 +1,9 @@
 import React from 'react'
 import { XStack, Stack } from  '@my/ui/src'
-import { SolitoImage } from 'solito/image'
+import { SolitoImage as Image } from 'solito/image'
 
 export const NavBar: React.FC = () => {
     return (
-        <XStack>
-            <SolitoImage
-            src="https://beatgig.com/image.pndg"
-            height={100}
-            width={100}
-            alt="A cool artist's imdadaage."
-            />
-        </XStack>
+        <Image src={"22222222"} alt={"222222222222222"} height={48} width={94} contentFit="fill" />
     )
 }
