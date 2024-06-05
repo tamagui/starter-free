3f0b2427bb19063b331f2d42ec6b2d43f43542bc
Daniagui12 <d.aguilera@uniandes.edu.co>
2024-06-04 21:17:18 -0500

    navbar prototype

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import { NavBar } from '@my/ui/src/components/NavBar'
+import { YStack, Text } from '@my/ui'
 
-export const HomeScreen: React.FC = () => 
-  {
-    return (
-       
-    )
-  }
+export const HomeScreen: React.FC = () => {
+  return (
+    <YStack width={'100%'} backgroundColor={"#1B1D24"} flex={1}>
+      <NavBar/>
+      <YStack width={'100%'} alignItems='flex-end'>
+        <Text fontSize={"$8"} fontWeight={"700"} paddingRight={50}>
+          Manga name
+        </Text>
+        <Text fontSize={18} width={"40%"}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua ut enim ad.
+        </Text>
+      </YStack>
+    </YStack>
+  )
+}
