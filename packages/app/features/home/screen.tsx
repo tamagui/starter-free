2e83d32af4fcfaedf95018c53428a15d30b095d2
Julian Mauricio Ferreira <jufeve28@gmail.com>
2024-06-08 15:51:54 -0500

    fix

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -4,16 +4,15 @@ import { YStack, Text } from '@my/ui'
 
 export const HomeScreen: React.FC = () => {
   return (
-    <YStack width={'100%'} backgroundColor={"#1B1D24"} flex={1}>
-      <NavBar/>
-      <YStack width={'100%'} alignItems='flex-end'>
-        <Text fontSize={"$8"} fontWeight={"700"} paddingRight={50}>
+    <YStack width={'100%'} backgroundColor={'#1B1D24'} flex={1}>
+      <NavBar />
+      <YStack width={'100%'} alignItems="flex-end">
+        <Text fontSize={'$8'} fontWeight={'700'} paddingRight={50}>
           Manga name
         </Text>
-        <Text fontSize={18} width={"40%"}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore magna
-          aliqua ut enim ad.
+        <Text fontSize={18} width={'40%'}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua ut enim ad.
         </Text>
       </YStack>
     </YStack>
