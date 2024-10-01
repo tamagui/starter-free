c7c7eab9281ce837e37f7f6974ce8e38b7fac251
jonsherrard <jon@devular.com>
2024-10-01 22:47:44 +0100

    biome to error on unused imports

--- apps/expo/scripts/fix-xcode-env.mjs
+++ apps/expo/scripts/fix-xcode-env.mjs
@@ -1,7 +1,6 @@
 import fs from 'node:fs/promises'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
-import { execSync } from 'node:child_process'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
