ad8697cd00bf6508bd7a7b545359507606e345f3
Bảo Hà <baronn.ha@gmail.com>
2025-03-17 02:28:28 +0700

    🔧 chore: Update ESLint settings and enhance theme handling in provider components

--- packages/app/provider/NextTamaguiProvider.tsx
+++ packages/app/provider/NextTamaguiProvider.tsx
@@ -5,7 +5,7 @@ import '@tamagui/font-inter/css/400.css'
 import '@tamagui/font-inter/css/700.css'
 import '@tamagui/polyfill-dev'
 
-import { useRef, type ReactNode } from 'react'
+import type { ReactNode } from 'react'
 import { useServerInsertedHTML } from 'next/navigation'
 import { NextThemeProvider, useRootTheme } from '@tamagui/next-theme'
 import { config } from '@my/ui'
@@ -14,37 +14,33 @@ import { Provider } from 'app/provider'
 export const NextTamaguiProvider = ({ children }: { children: ReactNode }) => {
   const [theme, setTheme] = useRootTheme()
 
-  const isServerInserted = useRef(false)
-
   useServerInsertedHTML(() => {
-    if (!isServerInserted.current) {
-      return (
-        <>
-          <style
-            dangerouslySetInnerHTML={{
-              // the first time this runs you'll get the full CSS including all themes
-              // after that, it will only return CSS generated since the last call
-              __html: config.getNewCSS(),
-            }}
-          />
+    return (
+      <>
+        <style
+          dangerouslySetInnerHTML={{
+            // the first time this runs you'll get the full CSS including all themes
+            // after that, it will only return CSS generated since the last call
+            __html: config.getNewCSS(),
+          }}
+        />
 
-          <style
-            dangerouslySetInnerHTML={{
-              __html: config.getCSS({
-                exclude: process.env.NODE_ENV === 'production' ? 'design-system' : null,
-              }),
-            }}
-          />
+        <style
+          dangerouslySetInnerHTML={{
+            __html: config.getCSS({
+              exclude: process.env.NODE_ENV === 'production' ? 'design-system' : null,
+            }),
+          }}
+        />
 
-          <script
-            dangerouslySetInnerHTML={{
-              // avoid flash of animated things on enter:
-              __html: `document.documentElement.classList.add('t_unmounted')`,
-            }}
-          />
-        </>
-      )
-    }
+        <script
+          dangerouslySetInnerHTML={{
+            // avoid flash of animated things on enter:
+            __html: `document.documentElement.classList.add('t_unmounted')`,
+          }}
+        />
+      </>
+    )
   })
 
   return (
